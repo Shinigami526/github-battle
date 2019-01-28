2ef0e7e8f362fdf3c1a804582f24058f5f385ac4
Shinigami526 <twitch7894@gmail.com>
2019-01-28 16:11:59 -0300

    add project

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import React, { component } from 'react';
+import Badge from './Badge';
+
+class Entry extends Component {
+  render() {
+    return(
+      <h1>Hola</h1>
+    )
+  }
+}
+export default Entry
